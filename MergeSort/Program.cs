adf6aa595ac8bbac28684686fff36ce51e1ce609
Gurmukh Hare <gurmukhhare@Gurmukhs-MacBook-Pro.local>
2021-10-10 22:09:11 -0700

    commiting lab2 project code Gurmukh Hare

--- MergeSort/Program.cs
+++ MergeSort/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace MergeSort
 {
@@ -7,60 +11,212 @@ namespace MergeSort
         static void Main(string[] args)
         {
 
-            ARRAY_SIZE = 1000;
+            int ARRAY_SIZE = 1000000;
             int[] arraySingleThread = new int[ARRAY_SIZE];
 
-
-
-
-            // TODO : Use the "Random" class in a for loop to initialize an array
+            //using 'Random' class to generate values for the array and placing those randomly generated numbers
+            Random rand = new Random();
+            for(int i = 0; i < ARRAY_SIZE; i++)
+            {
+                arraySingleThread[i] = rand.Next(0,1000); //generating random number between 0 and 999
+            }
 
             // copy array by value.. You can also use array.copy()
-            int[] arrayMultiThread = arraySingleThread.Slice(0,arraySingleThread.Length);
+            int[] arrayMultiThreadnew = new int[ARRAY_SIZE];
+            Array.Copy(arraySingleThread, arrayMultiThreadnew, ARRAY_SIZE);
+
 
             /*TODO : Use the  "Stopwatch" class to measure the duration of time that
                it takes to sort an array using one-thread merge sort and
                multi-thead merge sort
             */
+            Stopwatch myWatch = new Stopwatch();
 
-
-            //TODO :start the stopwatch
+            ////TODO :start the stopwatch
+            myWatch.Start();
             MergeSort(arraySingleThread);
-            //TODO :Stop the stopwatch
+            myWatch.Stop();
+            TimeSpan singleThreadTime = myWatch.Elapsed;
+            ////TODO :Stop the stopwatch
 
 
+            ////TODO: Multi Threading Merge Sort
+            ///
+            ///ALGORITHM:
+            //1) call method to split main unsorted array into 'N' subarrays. Store these subarrays in a separate array so we can iterate through and sort each
+            //2) loop through N subarrays and spin a new thread for each subarray
+            //3) in each new thread, send in the mergesort method as a param to sort the subarray
+            //4) we must wait for all threads to execute functionality using Join()
+            //5) merge N subarrays into one larger array and check if it is sorted -->  you have a queue of pairs of arrays that need to be merged, with each merged array also generating a new array that needs to be entered into the queue.
 
-            //TODO: Multi Threading Merge Sort
+
+            Stopwatch myWatchMultithreaded = new Stopwatch();
+            myWatchMultithreaded.Start();
+
+            int THREAD_COUNT = 2; //number of threads to be spun, one for each subarray
+            int[][] subarrays = GenerateSubarrays(THREAD_COUNT, arrayMultiThreadnew);
+            Thread[] threads = new Thread[THREAD_COUNT]; //store references to all the threads we are spinning, will reference these using indexing
+
+            for(int i = 0; i<THREAD_COUNT; i++) //spin 'THREAD_COUNT' threads and sort a subarray on each of these threads
+            {
+                int localIndex = i;
+                threads[localIndex] = new Thread(() => MergeSort(subarrays[localIndex]));
+                threads[localIndex].Start();
+            }
+
+            for(int i = 0; i < THREAD_COUNT; i++) //must Join each thread we spun so that we wait for all threads to finish sorting each subarray before merging
+            {
+                int localIndex = i;
+                threads[localIndex].Join();
+            }
+
+            //initializing a queue with all the sorted subarrays
+            Queue<int[]> queue = new Queue<int[]>(subarrays);
+
+            //keep popping pairs of sorted arrays from the queue, merging them, and pushing the result back into queue until only one fully sorted and merged array remains
+            while(queue.Count > 1)
+            {
+                int[] A = queue.Dequeue();
+                int[] B = queue.Dequeue();
+                int[] result = new int[A.Length + B.Length];
+
+                //merge arrays A and B and push the result back onto queue to be merged with another sorted subarray
+                Merge(A, B, result);
+                queue.Enqueue(result);
+            }
+
+            int[] sortedResult = queue.Dequeue(); //the final resulting sorted array using multithreading
+
+            myWatchMultithreaded.Stop();
+            TimeSpan multiThreadTime = myWatchMultithreaded.Elapsed;
+
+            bool isSortedSingle = IsSorted(arraySingleThread);
+            bool isSortedMulti = IsSorted(sortedResult);
+            Console.WriteLine(isSortedSingle);
+            Console.WriteLine(isSortedMulti);
+            Console.WriteLine(singleThreadTime);
+            Console.WriteLine(multiThreadTime);
 
 
+            /*Helper method to split original unsorted array into specified number of subarrays
+             * */
+            static int[][] GenerateSubarrays(int numThreads, int[] originalArray)
+            {
+                int[][] subarrays = new int[numThreads][];
+                int remainingLength = originalArray.Length; //keep track of how many elements in original array need to be placed
+                int remainingThreads = numThreads; // numThreads == number of subarrays we require
 
+                int i = 0; //to track the index of the original array
+                int k = 0; //to track index of the new 'subarrays' jagged array
+                while(remainingThreads > 0) //keep generating new subarrays until we have created a subarray for each thread
+                {
+                    int currSize = remainingLength / remainingThreads; //calculate how many elements should be placed in current subarray
+                    subarrays[k] = new int[currSize];
+                    for (int j = 0; j < currSize; j++) //iterate 'currSize' times to place 'currSize' items into subarray
+                    {
+                        subarrays[k][j] = originalArray[i];
+                        i++; //increment i so that the next element in the original array gets placed
+                    }
+                    k += 1; //increment k to set values for the next subarray in the next iteration
+                    remainingLength -= currSize;
+                    remainingThreads -= 1;
+                }
 
-
-
-
-             /*********************** Methods **********************
-              *****************************************************/
-             /*
-             implement Merge method. This method takes two sorted array and
-             and constructs a sorted array in the size of combined arrays
-             */
-
+                return subarrays;
+            }
+            /*********************** Methods **********************
+             *****************************************************/
+            /*
+            implement Merge method. This method takes two sorted array and
+            and constructs a sorted array in the size of combined arrays
+            */
             static int[] Merge(int[] LA, int[] RA, int[] A)
             {
 
-                // TODO :implement
+                int leftLength = LA.Length;
+                int rightLength = RA.Length;
+
+                int i = 0; //track index for left array
+                int j = 0; //track index for right array
+                int k = 0; //track index for array 'A' that will hold the resulting sorted elements
+
+                //looping through left and right arrays until one or both are out of bounds
+                while (i < leftLength && j < rightLength)
+                {
+                    if(LA[i] < RA[j])
+                    {
+                        A[k] = LA[i];
+                        i++;
+                    }
+                    else
+                    {
+                        A[k] = RA[j];
+                        j++;
+                    }
+                    k++;
+                }
+
+                //if either array still has items left to place in results array, loop through and place those elements
+                while(i < leftLength)
+                {
+                    A[k] = LA[i];
+                    i++;
+                    k++;
+                }
+
+                while(j < rightLength)
+                {
+                    A[k] = RA[j];
+                    j++;
+                    k++;
+                }
+
+                //return the result, which is the two arrays merged and sorted in ascending order
+                return A; 
+               
 
             }
 
 
-             /*
-             implement MergeSort method: takes an integer array by reference
-             and makes some recursive calls to intself and then sorts the array
-             */
+            // /*
+            // implement MergeSort method: takes an integer array by reference
+            // and makes some recursive calls to intself and then sorts the array
+            // */
             static int[] MergeSort(int[] A)
             {
+                //base case for the recursion
+                if (A.Length <= 1)
+                {
+                    return A;
+                }
 
-              // TODO :implement
+                //find midpoint of array to split original array into 2 halves
+                int length = A.Length;
+                int mid = length / 2;
+                int[] leftHalf = new int[mid];
+                int[] rightHalf = new int[length-mid];
+
+                //loop through the first 'mid' items in original array and store them into leftHalf array
+                for(int i = 0; i < mid; i++)
+                {
+                    leftHalf[i] = A[i];
+
+                }
+
+                //store remaining elements of original array into rightHalf array
+                int k = 0;
+                for (int j=mid; j < length; j++)
+                {
+                    rightHalf[k] = A[j];
+                    k++;
+                }
+
+                //sort each half with a recursive call 
+                int[] left = MergeSort(leftHalf);
+                int[] right = MergeSort(rightHalf);
+
+                //once both halves recursively sorted, merge the two halves into one array with correct ascending order
+                return Merge(left, right, A);
 
 
             }
@@ -94,6 +250,7 @@ namespace MergeSort
 
         }
 
+       
 
     }
 }
--- MultiThreadPi/MultiThreadPi/Program.cs
+++ MultiThreadPi/MultiThreadPi/Program.cs
@@ -1,25 +1,92 @@
 ﻿using System;
+using System.Threading;
+using System.Diagnostics;
+
 
 namespace MultiThreadPi
 {
     class MainClass
     {
+
         static void Main(string[] args)
         {
-            long numberOfSamples = 1000;
-            long hits;
-            double pi = EstimatePI(numberOfSamples, ref long hits);
+            int numThreads = Environment.ProcessorCount; //variable to adjust number of threads to generate samples on
+            long numberOfSamples = 10000000; //total number of samples to be divided into specified number of threads
+            long numberOfSamplesPerThread = numberOfSamples / numThreads;
+            long hits=0;
 
-            static double EstimatePI(long numberOfSamples, ref long hits)
+            Stopwatch myWatch = new Stopwatch();
+            Thread[] threads = new Thread[numThreads]; //to keep track of all the threads that are being spun
+            myWatch.Start();
+            //spin 'numThreads' threads and pass in EstimatePI method to each thread
+            for (int i = 0; i < numThreads; i++)
             {
-                //implement
+                int j = i;
+                threads[j] = new Thread(() => EstimatePI(numberOfSamplesPerThread, ref hits));
+                threads[j].Start();
             }
 
-            static double[,] GenerateSamples(long numberOfSamples)
+            //join all the threads that were spun to ensure we wait for each to finish generating samples and counting hits
+            for (int i = 0; i < numThreads; i++)
             {
-                // Implement  
-
+                int k = i;
+                threads[k].Join();
             }
+
+            //the pi value can be estimated by finding the ratio of hits to total number of samples and multiplying by 4
+            double pi = 4*((double)hits / (double)numberOfSamples);
+            myWatch.Stop();
+            TimeSpan calculatePItime = myWatch.Elapsed;
+            Console.WriteLine(pi);
+            Console.WriteLine(calculatePItime);
+            return;
+
+
+        }
+        static void EstimatePI(long numberOfSamples, ref long hits)
+        {
+            //jagged array to store all the generated coordinates after a function call to GenerateSamples 
+            double[][] samples = GenerateSamples(numberOfSamples);
+
+            //iterate through all coordinates generated and determine if distance is less than or equal to 1. If so, increment hits
+            for (int i = 0; i < numberOfSamples; i++)
+            {
+                double x = samples[i][0];
+                double y = samples[i][1];
+                if (Math.Sqrt(x * x + y * y) <= 1)
+                {
+                    Interlocked.Increment(ref hits);
+                }
+            }
+            return;
+        }
+
+        static double[][] GenerateSamples(long numberOfSamples)
+        {
+            //defining the range of our x and y coordinates
+            double min = -1.00;
+            double max = 1.00;
+
+            Random rand = new Random();
+            double[][] samples = new double[numberOfSamples][];
+
+            //generate the samples using the random generator
+            for (int i = 0; i < numberOfSamples; i++)
+            {
+                double x = rand.NextDouble() * (max - min) + min;
+                double y = rand.NextDouble() * (max - min) + min;
+                samples[i] = new double[] { x, y };
+            }
+            return samples;
         }
     }
 }
+
+//ALGORITHM:
+// 1) In main, set the size of our samples and the number of threads we'd like to use. Each thread will generate 'numberOfSamplesPerThread' samples
+// 2) use for loops to start and Join the threads. Each thread will run the 'EstimatePI' method
+// 2) use the generateSamples method strictly to generate the samples. An x and y coordinate, each between -1 and 1, will be generated 
+// 3) return the samples back to the EstimatePI method. Iterate through each coordinate and calculate distance. If distance <= 1, we increment 'hits'
+// 4) calculate the result in the main thread for PI. It will be 4*(hits/numerOfSamples)
+
+// NOTE: when we increment our 'hits', multiple threads will be trying to increment it. Make sure to use mutual exclusion
